8d22d11f87da6cf0e1f526806ec1cfda88f71565
harry moreno <morenoh149@gmail.com>
2012-01-19 20:42:47 -0500

    e-git

--- src/BFS.java
+++ src/BFS.java
@@ -7,9 +7,52 @@ import java.util.*;
  */
 public class BFS {
 	private Node currentNode;
-	private Queue frontier;
+	private Queue<Node> frontier;
+	private State goalState;
+	private String result;
+	
+	/**
+	 * initializes the BFS algorithm
+	 * @param initialState
+	 * @param goalState
+	 */
 	BFS(State initialState, State goalState){
+		this.goalState = goalState;
 		currentNode = new Node(0, null, initialState, null);
 		frontier = new LinkedList<Node>();
+		frontier.add(currentNode);
+		search(frontier);
+	}
+	
+	/**
+	 * searches the frontier in FIFO order
+	 * @param frontier
+	 */
+	public void search(Queue<Node> frontier){
+		this.currentNode = frontier.poll();
+		if(currentNode.getState().equals(goalState)){
+			getAncestry(currentNode);
+		}
+	}
+	/**
+	 * saves ancestry of the given node to result string
+	 * @param currentNode
+	 */
+	public void getAncestry(Node currentNode){
+		Node index = currentNode;
+		LinkedList<List<Integer>> reverseList = new LinkedList<List<Integer>>();
+		result = "";
+		while(index!=null){
+			reverseList.add(index.getMoved());
+			index = index.getParent();
+		}
+		while(!reverseList.isEmpty()){
+			List<Integer> l = reverseList.removeLast();
+			String temp = "";
+			for(Integer i : l){
+				temp.concat(i.toString()+" ");
+			}
+			result.concat(temp+"\n");
+		}
 	}
 }
